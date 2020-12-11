b9cf7afe811358e3d34b0a82b50b732c643426de
AndreCo17 <andrecyrilco@hotmail.com>
2020-12-10 17:28:15 -0700

    added classes for favorites, login and out nav bar

--- php/nav-header.php
+++ php/nav-header.php
@@ -24,8 +24,9 @@ require_once '../php/api/ASG2-classes.php';
                 <li><a href="about.php">About</a></li>
                 <li><a href="galleries.php">Galleries</a></li>
                 <li><a href="browse-paintings.php">Browse</a></li>
-                <li><a href="favorites.php">Favorites</a></li> <!-- only available when logged in -->
-                <li><a href="logout.php">Login/Logout</a></li>
+                <li><a class="logged-in" href="favorites.php">Favorites</a></li> <!-- only available when logged in -->
+                <li><a class="logged-in" href="logout.php">Logout</a></li>
+                <li><a class="logged-out" href="logout.php">Login</a></li>
             </ul>
             <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
             <li><a href="javascript:void(0);" class="icon" onclick="myFunction()"></li>
